9067b84a69a5d23b137d1d19d50008785271d804
Ambrose Chua <ambrose@chua.family>
2019-01-03 18:34:38 +0800

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+groups="cross-site-scripting image.php jinja-jail-break scrabble-quick-locator-interactive"
+
+for g in $groups; do
+	cd $g/
+	case "$1" in
+		""|start)
+			echo "Starting $g..."
+			sudo docker-compose up -d
+			;;
+		stop)
+			echo "Stopping $g..."
+			sudo docker-compose down
+			;;
+		reset)
+			echo "Resetting $g..."
+			sudo docker-compose down
+			sudo docker-compose up -d
+			;;
+		build)
+			echo "Rebuilding $g..."
+			sudo docker-compose build
+			sudo docker-compose down
+			sudo docker-compose up -d
+			;;
+	esac
+	cd ..
+done
